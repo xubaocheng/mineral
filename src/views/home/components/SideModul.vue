068e83350805d0e10cfcb33c26f6855515d0af63
Bates <xth5516739@163.com>
2021-03-02 22:31:42 +0800

    首页继续

---
+++ src/views/home/components/SideModul.vue
@@ -0,0 +1,93 @@
+<!-- 侧边推荐模块 -->
+<template>
+	<div class="side-modul">
+		<div class="side-modul-title clearFix" :style="{'background':background}">
+			<h2 class="left"><i class="el-icon-search" />{{ titletext }}</h2>
+			<span class="right">MORE</span>
+		</div>
+		<div class="side-modul-list">
+			<ul>
+				<li v-for="(item,index) in serverData" :key="`serverData_item_${index}`">
+					<p :class="{active: currentIndex === index}">{{ item.value }}</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SideModul',
+
+	components: {},
+
+	props: {
+		serverData: {
+			type: Array,
+			default: () => []
+		},
+		background: {
+			type: String,
+			default: () => ''
+		},
+		titletext: {
+			type: String,
+			default: () => ''
+		}
+	},
+	data() {
+		return {
+			currentIndex: 2
+		}
+	},
+
+	computed: {},
+
+	mounted() {},
+
+	methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+@import '~@/styles/home.scss';
+.side-modul{
+	width: 100%;
+	&-title{
+		width: 100%;
+		line-height: 55px;
+		padding: 0 10px;
+		background-color: $main-tone-color;
+		h2{
+			font-size: 22px;
+			font-weight: bold;
+			color: #fff;
+		}
+		span{
+			color: #fff;
+			font-size: 18px;
+		}
+	}
+	&-list{
+		width: 100%;
+		background-color: #fff;
+		ul{
+			li{
+				width: 100%;
+				p{
+
+					line-height: 20px;
+					padding: 5px 0;
+					padding-left: 30px;
+					cursor: pointer;
+					&.active{
+						background-color: #e6e9ee;
+						color: #1966aa;
+					}
+				}
+			}
+		}
+
+	}
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -38,7 +38,36 @@
 					<Listcom />
 				</div>
 				<div class="home-container-news-main-recommend right">
-					22
+					<SideModul :server-data="serverAnnalsData" background="#01274b" titletext="志书推荐" />
+					<SideModul :server-data="serverNewsData" background="#c4880e" titletext="新闻推荐" />
+				</div>
+			</div>
+		</div>
+		<div class="home-container-typical">
+			<div class="home-container-typical-main">
+				<div class="home-container-typical-main-title clearFix">
+					<em class="left" />
+					<div class="home-container-typical-main-title-text left">
+						<h2><span>典型</span><span class="active">矿床</span></h2>
+						<p>Typical Deposit</p>
+					</div>
+					<em class="left" />
+				</div>
+				<div class="home-container-typical-main-list clearFix">
+					<div v-for="(item,index) in typicalList" :key="`typicalList_item_${index}`" class="home-container-typical-main-list-item left">
+						<h3>{{ item.name }}</h3>
+						<p>{{ item.explain }}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="home-container-topicOverview">
+			<div class="home-container-topicOverview-main clearFix">
+				<div class="home-container-topicOverview-main-chart left">
+					sss
+				</div>
+				<div class="home-container-topicOverview-main-modul right">
+					www
 				</div>
 			</div>
 		</div>
@@ -48,17 +77,68 @@
 <script>
 import { mapGetters } from 'vuex'
 import TitleCom from './components/Title'
+import SideModul from './components/SideModul'
 import Listcom from '_c/ListCom'
 export default {
 	name: 'Home',
 
 	components: {
 		TitleCom,
-		Listcom
+		Listcom,
+		SideModul
 	},
 
 	data() {
 		return {
+			serverAnnalsData: [
+				{
+					value: '云南勐海勐往独居石沙矿床'
+				},
+				{
+					value: '典型矿床地质志'
+				},
+				{
+					value: '中国矿产地质志'
+				},
+				{
+					value: '中国矿产地质志2019年'
+				},
+				{
+					value: '华北矿产矿产概况总述卷'
+				}
+			],
+			serverNewsData: [
+				{
+					value: '云南勐海勐往独居石沙矿床云南勐海勐往独居石沙矿床'
+				},
+				{
+					value: '地质志2019年工作会议召开地质志书2019年工作会议召开'
+				},
+				{
+					value: '云南勐海勐往独居石沙矿床云南勐海勐往独居石沙矿床'
+				},
+				{
+					value: '云南勐海勐往独居石沙矿床云南勐海勐往独居石沙矿床'
+				},
+				{
+					value: '云南勐海勐往独居石沙矿床云南勐海勐往独居石沙矿床'
+				}
+			],
+			typicalList: [
+				{
+					name: '珠宝玉石',
+					explain: '矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明'
+				}, {
+					name: '矿产名称',
+					explain: '矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明'
+				}, {
+					name: '矿产名称',
+					explain: '矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明'
+				}, {
+					name: '矿产名称',
+					explain: '矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明矿产简介说明'
+				}
+			]
 		}
 	},
 
@@ -149,6 +229,104 @@ export default {
 				}
 			}
 
+		}
+		&-typical{
+			width: 100%;
+			padding-bottom: 60px;
+			&-main{
+				width: 1170px;
+				margin: 0 auto;
+				&-title{
+					width: 500px;
+					padding: 30px 0;
+					margin: 0 auto;
+					em{
+						width: 125px;
+						margin: 15px 0;
+						height: 2px;
+						background-color: #c4880e;
+					}
+					&-text{
+						text-align: center;
+						padding: 0 50px;
+						h2{
+							padding-bottom: 5px;
+							span{
+								font-size: 26px;
+								font-weight: bold;
+								color: #050505;
+								&.active{
+									color: #c4880c;
+								}
+							}
+						}
+						p{
+							text-align: center;
+							font-size: 16px;
+							color: #828282;
+						}
+					}
+				}
+				&-list{
+					width: 100%;
+					&-item{
+						width: 25%;
+						height:220px;
+						background: url('../../assets/img/test2.png') no-repeat;
+						cursor: pointer;
+						color: #fff;
+						position: relative;
+						transition:all 1s ease-out;
+						h3{
+							width: 100%;
+							position: absolute;
+							left: 0;
+							bottom: 0;
+							font-size: 28px;
+							font-weight: bold;
+							background-color: #0c243c;
+							opacity: 0.7;
+							text-align: center;
+							line-height: 60px;
+						}
+						p{
+							padding:0 35px;
+							position: absolute;
+							left: 0;
+							top: 60px;
+							line-height: 30px;
+							font-weight: bold;
+							display: none;
+						}
+						&:hover{
+							transform:scale(1.2);
+							z-index: 2;
+							h3{
+								top: 0;
+							}
+							p{
+								display: block;
+							}
+						}
+					}
+				}
+			}
+		}
+		&-topicOverview{
+			width: 100%;
+			background: #f3f3f3;
+			&-main{
+				width: 1170px;
+				margin: 0 auto;
+				padding: 50px 0;
+				height: 660px;
+				&-chart{
+					width: 780px;
+				}
+				&-modul{
+					width: 360px;
+				}
+			}
 		}
 	}
 }
